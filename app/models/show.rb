0fcfbe3eb3cadff1ad6daaebf8169c8b2fb79deb
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-19 13:36:35 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.name.where("rating = ?", highest_rating)
+    self.where("rating = ?", highest_rating).name
   end
   
   def self.lowest_rating
