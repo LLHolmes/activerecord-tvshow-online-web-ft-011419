d8685634dbd5c639d97a15ff54778149d4b5e608
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-19 13:38:20 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
   end  
   
   def self.least_popular_show
-    lowest_rating.name
+    self.where("rating = ?", lowest_rating)[0]
   end
   
   def self.ratings_sum
